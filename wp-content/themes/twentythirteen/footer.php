24348e460267855789ddb19052fea376ddc07ba5
David Regal <dvd.regal@gmail.com>
2014-01-21 14:27:40 -0500

    Restored original files.

--- wp-content/themes/twentythirteen/footer.php
+++ wp-content/themes/twentythirteen/footer.php
@@ -15,7 +15,8 @@
 			<?php get_sidebar( 'main' ); ?>
 
 			<div class="site-info">
-				<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'startup' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); ?>
+				<?php do_action( 'twentythirteen_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->

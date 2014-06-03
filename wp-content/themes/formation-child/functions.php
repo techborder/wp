e49d409c7a41e3185fd3c500b7bd7929e3c9f95f
David Regal <dvd.regal@gmail.com>
2014-06-03 19:23:37 -0400

    Code cleanup and add 2 more paragraphs to feature text.

--- wp-content/themes/formation-child/functions.php
+++ wp-content/themes/formation-child/functions.php
@@ -26,13 +26,60 @@ add_action( 'wp_enqueue_scripts', 'Formation_child_scripts' );
 
 
 /**
- * Adds the individual section for featured text box top
+ * Adds a paragraph in the featured text
  */
-function formation_customizer_header_text_area( $wp_customize ) {
+//function formation_customizer_header_text_area( $wp_customize ) {
+//	
+//	$wp_customize->add_section( 'featured_section_paragraph_one', array(
+//	'title'       => __( 'Featured Text Area Paragraph 1', 'Formation Child' ),
+//	'description' => __( 'This is a settings section to add paragraphs below the Feature Text title on the homepage featured text area.', 'Formation' ),
+//	'priority' => 166,
+//	)
+//	);
+//	
+//	$wp_customize->add_setting(
+//		'featured_text_paragraph_one', array(
+//		'default' => __( 'Default Featured Paragraph 1', 'Formation Child' ),
+//		'sanitize_callback' => 'Formation_sanitize_text',
+//		)
+//	);
+//	
+//	$wp_customize->add_control(
+//		'featured_text_paragraph_one', array(
+//		'label'    => __( 'Featured Text Paragraph 1', 'Formation Child' ),
+//		'section' => 'featured_section_paragraph_one',
+//		'type' => 'text',
+//		)
+//	);
+//	
+//}
+//add_action( 'customize_register', 'formation_customizer_header_text_area' );
+//
+///**
+// * Adds a paragraph in the featured text
+// */
+//function formation_customizer_header_text_area( $wp_customize ) {
+//	
+//	$wp_customize->add_section( 'featured_section_paragraphs', array(
+//	'title'       => __( 'Featured Text Area Paragraph 1', 'Formation Child' ),
+//	'description' => __( 'This is a settings section to add paragraphs below the Feature Text title on the homepage featured text area.', 'Formation' ),
+//	'priority' => 166,
+//	)
+//	);
+//	
+//	
+//}
+//add_action( 'customize_register', 'formation_customizer_header_text_area' );
+
+
+/**
+ * Adds a paragraph in the featured text
+ */
+function feature_text_paragraphs_customizer( $wp_customize ) {
 	
-	$wp_customize->add_section( 'featured_section_paragraph_one', array(
-	'title'       => __( 'Featured Text Area Paragraph 1', 'Formation Child' ),
-	'description' => __( 'This is a settings section to add paragraphs below the Feature Text title on the homepage featured text area.', 'Formation' ),
+	$wp_customize->add_section( 'featured_section_paragraphs', array(
+	'title'       => __( 'Featured Text Area Paragraphs', 'Formation Child' ),
+	'description' => __( 'Adds paragraphs below the Feature Text title on the homepage featured text area.', 'Formation' ),
 	'priority' => 166,
 	)
 	);
@@ -47,12 +94,40 @@ function formation_customizer_header_text_area( $wp_customize ) {
 	$wp_customize->add_control(
 		'featured_text_paragraph_one', array(
 		'label'    => __( 'Featured Text Paragraph 1', 'Formation Child' ),
-		'section' => 'featured_section_paragraph_one',
+		'section' => 'featured_section_paragraphs',
+		'type' => 'text',
+		)
+	);
+	
+	$wp_customize->add_setting(
+		'featured_text_paragraph_two', array(
+		'default' => __( 'Default Featured Paragraph 2', 'Formation Child' ),
+		'sanitize_callback' => 'Formation_sanitize_text',
+		)
+	);
+	
+	$wp_customize->add_control(
+		'featured_text_paragraph_two', array(
+		'label'    => __( 'Featured Text Paragraph 2', 'Formation Child' ),
+		'section' => 'featured_section_paragraphs',
+		'type' => 'text',
+		)
+	);
+	
+	$wp_customize->add_setting(
+		'featured_text_paragraph_three', array(
+		'default' => __( 'Default Featured Paragraph 3', 'Formation Child' ),
+		'sanitize_callback' => 'Formation_sanitize_text',
+		)
+	);
+	
+	$wp_customize->add_control(
+		'featured_text_paragraph_three', array(
+		'label'    => __( 'Featured Text Paragraph 3', 'Formation Child' ),
+		'section' => 'featured_section_paragraphs',
 		'type' => 'text',
 		)
 	);
 	
 }
-add_action( 'customize_register', 'formation_customizer_header_text_area' );
-
-// TODO: Add feature text paragraphs 2 and 3.
+add_action( 'customize_register', 'feature_text_paragraphs_customizer' );

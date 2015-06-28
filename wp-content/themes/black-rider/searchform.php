d4a7f74c56fdb993e769c0a4fe747cc1801b07e3
David Regal <dvd.regal@gmail.com>
2015-06-28 12:19:16 -0600

    Cleanup. Deleted unused themes.

--- wp-content/themes/black-rider/searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
-    <div>
-        <input type="text" onfocus="if (this.value == '<?php _e('Search','blcr'); ?>') {this.value = '';}" onblur="if (this.value == '') {this.value = '<?php _e('Search','blcr'); ?>';}"  value="<?php _e('Search','blcr'); ?>" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="" />
-    </div>
-</form>
-<div class="clear"></div>
--- wp-content/themes/startupwp/entry-summary.php
+++
@@ -1,7 +0,0 @@
-<div class="entry-summary">
-<?php the_excerpt( sprintf(__( 'continue reading %s', 'startup' ), '<span class="meta-nav">&rarr;</span>' ) ); ?>
-<?php if(is_search()) {
-wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'startup' ) . '&after=</div>');
-}
-?>
-</div>
